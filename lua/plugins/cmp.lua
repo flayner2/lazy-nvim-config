d52b71684da40d83601b292db44666f215a03494
Maycon Oliveira <flayner5@gmail.com>
2024-06-17 21:00:48 -0300

    Fixed some behaviour with supertab

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,11 +8,11 @@ return {
     opts = function(_, opts)
       -- Enable cmp-emoji
       table.insert(opts.sources, { name = "emoji" })
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
+      -- local has_words_before = function()
+      --   unpack = unpack or table.unpack
+      --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      -- end
 
       local cmp = require("cmp")
 
@@ -28,8 +28,8 @@ return {
             vim.schedule(function()
               vim.snippet.jump(1)
             end)
-          elseif has_words_before() then
-            cmp.complete()
+          -- elseif has_words_before() then
+          --   cmp.complete()
           else
             fallback()
           end
